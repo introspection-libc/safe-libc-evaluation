598a440a1edffcf73d8bf8c72d339f65978d89a8
Manuel Rigger <manuel.rigger@jku.at>
2018-02-02 12:15:38 +0100

    Check in versions of the projects that use MPX without introspection (for benchmarking)

---
+++ cve/mpx-no-introspection/LightFTP-1.1/Source/Other/Release/subdir.mk
@@ -0,0 +1,30 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../cfgparse.c \
+../ftpserv.c \
+../main.c 
+
+OBJS += \
+./cfgparse.o \
+./ftpserv.o \
+./main.o 
+
+C_DEPS += \
+./cfgparse.d \
+./ftpserv.d \
+./main.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+%.o: ../%.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C Compiler'
+	$(CC) $(CFLAGS) -std=c99 -O3 -Wall -Wextra -c -fmessage-length=0 -Wno-unused-parameter -Wno-unused-result -fno-ident -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
